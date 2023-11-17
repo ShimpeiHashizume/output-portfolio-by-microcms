287e357f4a646a6f7773fc612cdb8ff9cdb93d1f
hashizume <hashizume@fabric-arts.co.jp>
2023-11-17 14:25:31 +0900

    [update]ナビゲーションのデータ作成

---
+++ src/constants/NavLinks.ts
@@ -0,0 +1,17 @@
+export const navList = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 3,
+    title: "About",
+    url: "/about",
+  },
+];
