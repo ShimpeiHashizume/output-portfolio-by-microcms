4596b2a72bc82867281328a1ef04ee8211bd4afb
hashizume <hashizume@fabric-arts.co.jp>
2023-11-17 14:57:17 +0900

    [update]Aboutページ作成

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import PageTitle from "@/component/PageTitle/PageTitle";
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <PageTitle title="About" desc="プロフィールページ" />
+    </div>
+  );
+};
+
+export default About;
