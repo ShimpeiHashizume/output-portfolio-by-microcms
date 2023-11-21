5d3c54a83da5d580ec1bc60683ba27484c51cb02
hashizume <hashizume@fabric-arts.co.jp>
2023-11-21 16:16:21 +0900

    [update]カテゴリ追加（静的）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,43 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import PageTitle from "@/component/PageTitle/PageTitle";
+import Link from "next/link";
+
+const tagData = [
+  {
+    name: "JavaScript",
+    url: "javascript",
+  },
+  {
+    name: "React",
+    url: "react",
+  },
+  {
+    name: "Next.js",
+    url: "next.js",
+  },
+  {
+    name: "Tailwindcss",
+    url: "tailwindcss",
+  },
+  {
+    name: "Storybook",
+    url: "storybook",
+  },
+];
 
 export default function Home() {
-  return <div>aaaaa</div>;
+  return (
+    <div className="mainBlock">
+      <PageTitle title="Output Code" desc="アウトプット用のポートフォリオサイト" />
+      <ul className={styles.tagList}>
+        {tagData.map((tag) => (
+          <li key={tag.url} className={styles.tagItem}>
+            <Link href={tag.url} className={styles.tagItemLink}>
+              {tag.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
