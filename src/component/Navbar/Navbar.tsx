aff8cf41a923a26f97ff72530cca6c9e94e36599
hashizume <hashizume@fabric-arts.co.jp>
2023-11-21 13:35:48 +0900

    [update]カレント機能追加

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -1,10 +1,14 @@
+"use client";
+
 import React from "react";
 import styles from "./Navbar.module.css";
 import Link from "next/link";
 import { navList } from "@/constants/NavLinks";
 import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
   return (
     <header className={styles.header}>
       <div className={styles.headerInner}>
@@ -17,7 +21,7 @@ const Navbar = () => {
           <ul className={styles.navigationList}>
             {navList.map((item) => (
               <li key={item.id} className={styles.navigationItem}>
-                <Link href={item.url} className={styles.navigationItemLink}>
+                <Link href={item.url} className={`${styles.navigationItemLink} ${pathname === item.url && styles.current}`}>
                   {item.title}
                 </Link>
               </li>
