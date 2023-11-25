9fda7dd44f2c9f9cb81b6b724c57b5cad7cdec71
hashizume <hashizume@fabric-arts.co.jp>
2023-11-25 11:36:54 +0900

    [update]blogページ作成

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,12 @@
+import PageTitle from "@/component/PageTitle/PageTitle";
+import React from "react";
+
+const Blog = async () => {
+  return (
+    <div className="mainBlock">
+      <PageTitle title="Blog" desc="ブログ" />
+    </div>
+  );
+};
+
+export default Blog;
