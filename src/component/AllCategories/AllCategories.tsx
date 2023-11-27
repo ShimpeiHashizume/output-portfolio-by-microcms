f63e63fd1abe340a716c75d98f23918c75364d60
hashizume <hashizume@fabric-arts.co.jp>
2023-11-27 16:49:45 +0900

    [update]カテゴリコンポーネントを作成

---
+++ src/component/AllCategories/AllCategories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { categoriesProps } from "@/type/type";
+import styles from "./AllCategories.module.css";
+import Link from "next/link";
+
+const AllCategories = ({ categories }: { categories: categoriesProps[] }) => {
+  return (
+    <ul className={styles.tagList}>
+      {categories.map((category: categoriesProps) => (
+        <li key={category.id} className={styles.tagItem}>
+          <Link href={`/category/${category.slug}`} className={styles.tagItemLink}>
+            {category.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default AllCategories;
