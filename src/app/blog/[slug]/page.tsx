392c22700e3acfef902822aa4341921e8a1ba424
hashizume <hashizume@fabric-arts.co.jp>
2023-11-27 13:14:55 +0900

    [update]記事詳細ページにコンポーネント追加

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./page.module.css";
 import { getPostBySlug } from "@/lib/api";
 import PostHeader from "@/component/PostHeader/PostHeader";
+import ConvertBody from "@/component/ConvertBody/ConvertBody";
+import PostBody from "@/component/PostBody/PostBody";
 
 const Detail = async () => {
   const slug = "nexttypescript";
@@ -12,6 +14,9 @@ const Detail = async () => {
     <div className="mainBlock">
       <div className={styles.inner}>
         <PostHeader title={post.title} publish={post.publishDate} />
+        <PostBody>
+          <ConvertBody contentHTML={post.contents} />
+        </PostBody>
       </div>
     </div>
   );
