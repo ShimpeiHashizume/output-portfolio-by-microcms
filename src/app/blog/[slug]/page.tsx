35108a576a7ee03c4ebdda692b7880ab5f1ead92
hashizume <hashizume@fabric-arts.co.jp>
2023-11-28 11:28:07 +0900

    [update]レスポンシブ対応

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -17,7 +17,7 @@ const Detail = async ({ params }: { params: { slug: string } }) => {
         <PostBody>
           <ConvertBody contentHTML={post.contents} />
         </PostBody>
-        <Pagination prevText="記事一覧に戻る" prevUrl="/blog" nextText="記事一覧に進む" nextUrl="/blog" />
+        <Pagination prevText="記事一覧に戻る" prevUrl="/blog" />
       </div>
     </div>
   );
