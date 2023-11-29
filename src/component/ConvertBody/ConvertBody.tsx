8e3a5f1c68f3f6552b368073eb90ba4581e3c323
hashizume <hashizume@fabric-arts.co.jp>
2023-11-29 14:58:30 +0900

    [update]コードのシンタックスハイライト

--- src/component/ConvertBody/ConvertBody.tsx
+++ src/component/ConvertBody/ConvertBody.tsx
@@ -1,16 +1,30 @@
 import { ConvertBodyProps } from "@/type/type";
-import parse, { Element } from "html-react-parser";
+import hljs from "highlight.js";
+import "highlight.js/styles/atom-one-dark.css";
+import parse, { Element, Text } from "html-react-parser";
+
 import Image from "next/image";
 import React from "react";
 
 const ConvertBody: React.FC<ConvertBodyProps> = ({ contentHTML }) => {
   const contentReact = parse(contentHTML, {
     replace: (node) => {
-      const { name, attribs } = node as Element;
-      if (name === "img" && attribs) {
-        const { src, alt, width, height } = attribs;
-        return <Image src={src} alt={alt} width={Number(width)} height={Number(height)} />;
+      if (node instanceof Element) {
+        const { name, attribs } = node;
+        if (name === "img" && attribs) {
+          const { src, alt, width, height } = attribs;
+          return <Image src={src} alt={alt} width={Number(width)} height={Number(height)} />;
+        }
+        if (name === "code") {
+          const textNode = node.children.find((child) => child instanceof Text) as Text | undefined;
+          if (textNode) {
+            const result = hljs.highlightAuto(textNode.data);
+            const dom = parse(result.value);
+            return <code className="hljs">{dom}</code>;
+          }
+        }
       }
+      return node;
     },
   });
   return <>{contentReact}</>;
